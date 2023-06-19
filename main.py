8ed1ab815c12f0e061aab677fc8deb19a0aa4310
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-06-19 16:35:29 +0300

    ...

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello dunyo!</h1>"
 
+if __name__ == '__main__':
+   app.run(debug = True)
