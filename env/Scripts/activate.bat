083bc58e08ca5a0c0cf889d14cdc49c7027a52b5
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-06-21 15:12:41 +0300

    ...

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\O'quvchi\Desktop\FLASK_APP\env
+set VIRTUAL_ENV=C:\Users\IT CENTER KARMANA\Desktop\Flask_APP\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello dunyo!</h1>"
+def home():
+    return render_template('index.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
